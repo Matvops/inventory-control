eb8db1a4a5015ff373add878580abddc773bc107
Matheus <matheusmartinscia@gmail.com>
2024-11-28 22:48:29 -0300

    Enums modified

--- src/main/java/com/cadenassi/inventory_control/enums/CategoryEnum.java
+++ src/main/java/com/cadenassi/inventory_control/enums/CategoryEnum.java
@@ -5,32 +5,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum CategoryEnum {
-    BLUSA(0),
-    BONE(1),
-    CROPPED(2),
-    VESTIDO(3),
-    CHINELO(4),
-    JAQUETA(5),
-    CALCA(6),
-    CAMISA(7),
-    SHORTS(8),
-    SAIA(9),
-    TENIS(10),
-    MEIA(11),
-    MOLETOM(12),
-    REGATA(13),
-    CAMISETA(14),
-    INTIMA(15);
-
-    private int i;
-
-    private CategoryEnum(int i) {
-        this.i = i;
-    }
-
-    public int getI() {
-        return i;
-    }
+    BLUSA,
+    BONE,
+    CROPPED,
+    VESTIDO,
+    CHINELO,
+    JAQUETA,
+    CALCA,
+    CAMISA,
+    SHORTS,
+    SAIA,
+    TENIS,
+    MEIA,
+    MOLETOM,
+    REGATA,
+    CAMISETA,
+    INTIMA;
 
     public CategoryEnum getCategory(int i){
         List<CategoryEnum> categories = new ArrayList<>(Arrays.asList(CategoryEnum.values()));
--- src/main/java/com/cadenassi/inventory_control/enums/MaterialEnum.java
+++ src/main/java/com/cadenassi/inventory_control/enums/MaterialEnum.java
@@ -5,26 +5,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum MaterialEnum {
-    ALGODAO(0),
-    COURO(1),
-    DRY_FIT(2),
-    JEANS(3),
-    SOCIAL(4),
-    MOLETOM(5),
-    POLIESTER(6),
-    PRIMEIRA_LINHA(7),
-    SEGUNDA_LINHA(8),
-    TACTEL(9);
-
-    private int i;
-
-    private MaterialEnum(int i) {
-        this.i = i;
-    }
-
-    public int getI() {
-        return i;
-    }
+    ALGODAO,
+    COURO,
+    DRY_FIT,
+    JEANS,
+    SOCIAL,
+    MOLETOM,
+    POLIESTER,
+    PRIMEIRA_LINHA,
+    SEGUNDA_LINHA,
+    TACTEL;
 
     public MaterialEnum getMaterial(int i){
         List<MaterialEnum> materials = new ArrayList<>(Arrays.asList(MaterialEnum.values()));
