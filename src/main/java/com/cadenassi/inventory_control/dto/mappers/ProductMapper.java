283b98a1570f05254fe805acfd1089cdd207b8da
Matheus <matheusmartinscia@gmail.com>
2024-11-29 22:59:03 -0300

    Product Mapper Test

--- src/main/java/com/cadenassi/inventory_control/dto/mappers/ProductMapper.java
+++ src/main/java/com/cadenassi/inventory_control/dto/mappers/ProductMapper.java
@@ -3,11 +3,14 @@ package com.cadenassi.inventory_control.dto.mappers;
 import com.cadenassi.inventory_control.dto.objects.ProductDTO;
 import com.cadenassi.inventory_control.model.product.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
     Product toProduct(ProductDTO productDTO);
 
--- src/test/java/com/cadenassi/inventory_control/unittests/mappers/ProductMapperTest.java
+++ src/test/java/com/cadenassi/inventory_control/unittests/mappers/ProductMapperTest.java
@@ -1,4 +1,83 @@
 package com.cadenassi.inventory_control.unittests.mappers;
 
+import com.cadenassi.inventory_control.dto.mappers.ProductMapper;
+import com.cadenassi.inventory_control.enums.CategoryEnum;
+import com.cadenassi.inventory_control.enums.ClothingEnum;
+import com.cadenassi.inventory_control.enums.MaterialEnum;
+import com.cadenassi.inventory_control.unittests.mappers.mocks.MockProduct;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProductMapperTest {
+
+    private MockProduct mock;
+
+    private ProductMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mock = new MockProduct();
+        mapper = ProductMapper.INSTANCE;
+    }
+
+    @Test
+    @Order(0)
+    void productToProductDTOTest() {
+        var product = mock.getProduct(1);
+        var dto = mapper.toProductDTO(product);
+
+        assertEquals("PRODUCT1", dto.getName());
+        assertEquals(10, dto.getPrice());
+        assertEquals(10, dto.getQuantity());
+        assertEquals(ClothingEnum.getClothing(1), dto.getClothing());
+        assertEquals(CategoryEnum.getCategory(1), dto.getCategory().getCategory());
+        assertEquals(MaterialEnum.getMaterial(1), dto.getCategory().getMaterial());
+    }
+
+    @Test
+    @Order(1)
+    void productDTOToProductTest() {
+        var dto = mock.getProductDTO(1);
+        var product = mapper.toProduct(dto);
+
+        assertEquals("PRODUCT1", product.getName());
+        assertEquals(10, product.getPrice());
+        assertEquals(10, product.getQuantity());
+        assertEquals(ClothingEnum.getClothing(1), product.getClothing());
+        assertEquals(CategoryEnum.getCategory(1), product.getCategory().getCategory());
+        assertEquals(MaterialEnum.getMaterial(1), product.getCategory().getMaterial());
+    }
+
+    @Test
+    @Order(2)
+    void listProductToListProductDTOTest() {
+        var products = mock.getListProduct();
+        var dtos = mapper.toListProductDTO(products);
+
+        for(int i = 0; i < dtos.size(); i++){
+            assertEquals("PRODUCT"+i, dtos.get(i).getName());
+            assertEquals(10 * i, dtos.get(i).getPrice());
+            assertEquals(10 * i, dtos.get(i).getQuantity());
+            assertEquals(ClothingEnum.getClothing(i), dtos.get(i).getClothing());
+            assertEquals(CategoryEnum.getCategory(i), dtos.get(i).getCategory().getCategory());
+            assertEquals(MaterialEnum.getMaterial(i), dtos.get(i).getCategory().getMaterial());
+        }
+    }
+
+    @Test
+    @Order(3)
+    void listProductDTOToListProductTest() {
+        var dtos = mock.getListProductDTO();
+        var products = mapper.toListProduct(dtos);
+
+        for(int i = 0; i < dtos.size(); i++){
+            assertEquals("PRODUCT"+i, products.get(i).getName());
+            assertEquals(10 * i, products.get(i).getPrice());
+            assertEquals(10 * i, products.get(i).getQuantity());
+            assertEquals(ClothingEnum.getClothing(i), products.get(i).getClothing());
+            assertEquals(CategoryEnum.getCategory(i), products.get(i).getCategory().getCategory());
+            assertEquals(MaterialEnum.getMaterial(i), products.get(i).getCategory().getMaterial());
+        }
+    }
 }
