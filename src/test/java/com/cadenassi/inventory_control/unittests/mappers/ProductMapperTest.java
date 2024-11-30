eb27208e142d8a9bd0701f4d47aeb648a6093b3c
Matheus <matheusmartinscia@gmail.com>
2024-11-29 21:35:30 -0300

    ProductMock created

---
+++ src/test/java/com/cadenassi/inventory_control/unittests/mappers/ProductMapperTest.java
@@ -0,0 +1,4 @@
+package com.cadenassi.inventory_control.unittests.mappers;
+
+public class ProductMapperTest {
+}
---
+++ src/test/java/com/cadenassi/inventory_control/unittests/mappers/mocks/MockProduct.java
@@ -0,0 +1,4 @@
+package com.cadenassi.inventory_control.unittests.mappers.mocks;
+
+public class ProductMock {
+}
