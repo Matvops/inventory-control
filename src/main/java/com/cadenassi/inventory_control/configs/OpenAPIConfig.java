4a5478ecefdf31be5ab74ec14e3d6a93bebe50bd
Matheus <matheusmartinscia@gmail.com>
2024-12-18 22:14:43 -0300

    Swagger has been added

---
+++ src/main/java/com/cadenassi/inventory_control/configs/OpenAPIConfig.java
@@ -0,0 +1,22 @@
+package com.cadenassi.inventory_control.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+
+public class OpenAPIConfig {
+
+    @Bean
+    OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("INVENTORY CONTROL DOCUMENTATION")
+                        .summary("DOCUMENTATION OF REQUESTS TO CONTROLLERS")
+                        .description("THIS DOCUMENTATION DOCUMENTS ALL AVAILABLE RESOURCES")
+                        .version("V1")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
