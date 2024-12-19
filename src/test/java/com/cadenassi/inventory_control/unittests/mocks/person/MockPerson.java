94803a7dbf03f578e9cc8f51caa2424e094483ae
Matheus <matheusmartinscia@gmail.com>
2024-12-18 21:46:26 -0300

    Unit test for PersonService have been implemented

--- src/test/java/com/cadenassi/inventory_control/unittests/mocks/person/MockPerson.java
+++ src/test/java/com/cadenassi/inventory_control/unittests/mocks/person/MockPerson.java
@@ -1,4 +1,85 @@
 package com.cadenassi.inventory_control.unittests.mocks.person;
 
+import com.cadenassi.inventory_control.dto.objects.PersonDTO;
+import com.cadenassi.inventory_control.model.person.Client;
+import com.cadenassi.inventory_control.model.person.Employee;
+import com.cadenassi.inventory_control.model.person.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MockPerson {
+
+
+    public Person getPerson(String type) {
+        return getPerson("00000000000", type);
+    }
+
+    public PersonDTO getPersonDTO() {
+        return getPersonDTO("00000000000");
+    }
+
+    public Person getPerson(String cpf, String type) {
+        Person person = null;
+
+        if("EMPLOYEE".equalsIgnoreCase(type)) {
+            person = new Employee();
+            person.setCpf(cpf);
+            person.setName("NAME" + cpf);
+            person.setPhoneNumber("NUMBER" + cpf);
+
+        }
+
+        if("CLIENT".equalsIgnoreCase(type)){
+            person = new Client();
+            person.setCpf(cpf);
+            person.setName("NAME" + cpf);
+            person.setPhoneNumber("NUMBER" + cpf);
+        }
+
+
+
+        return person;
+    }
+
+
+    public PersonDTO getPersonDTO(String cpf) {
+        var dto = new PersonDTO();
+
+        dto.setCpf(cpf);
+        dto.setName("NAME" + cpf);
+        dto.setPhoneNumber("NUMBER" + cpf);
+
+        return dto;
+    }
+
+    public List<? extends Person> getListPerson(String type) {
+        List<Person> persons = new ArrayList<>();
+        String cpf;
+
+        for (int i = 0; i < 10; i++) {
+            cpf = "";
+            for(int j = 0; j < 11; j++){
+                cpf += i;
+            }
+            persons.add(getPerson(cpf, type));
+        }
+
+        return persons;
+    }
+
+    public List<PersonDTO> getPersonDTOs(){
+        List<PersonDTO> dtos = new ArrayList<>();
+        String cpf;
+
+        for (int i = 0; i < 10; i++) {
+            cpf = "";
+            for (int j = 0; j < 11; j++) {
+                cpf += i;
+            }
+            dtos.add(getPersonDTO(cpf));
+        }
+
+        return dtos;
+    }
 }
--- src/test/java/com/cadenassi/inventory_control/unittests/services/PersonServiceTest.java
+++ src/test/java/com/cadenassi/inventory_control/unittests/services/PersonServiceTest.java
@@ -1,4 +1,105 @@
 package com.cadenassi.inventory_control.unittests.services;
 
+import com.cadenassi.inventory_control.dto.mappers.PersonMapper;
+import com.cadenassi.inventory_control.dto.objects.PersonDTO;
+import com.cadenassi.inventory_control.model.person.Client;
+import com.cadenassi.inventory_control.model.person.Employee;
+import com.cadenassi.inventory_control.model.person.Person;
+import com.cadenassi.inventory_control.repositories.person.PersonRepository;
+import com.cadenassi.inventory_control.services.person.PersonServiceImpl;
+import com.cadenassi.inventory_control.unittests.mocks.person.MockPerson;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PersonServiceTest {
+
+    @Mock
+    PersonMapper mapper;
+
+    @Mock
+    PersonRepository repository;
+
+    @InjectMocks
+    PersonServiceImpl service;
+
+    MockPerson mock;
+
+    @BeforeEach
+    void setUp() {
+        mock = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    @Order(0)
+    void getPersonByCpf(){
+        //ARRANGE
+        var cpf = "00000000000";
+        Person person = mock.getPerson(cpf, "EMPLOYEE");
+        PersonDTO dto = mock.getPersonDTO(cpf);
+
+        when(repository.getPersonByCpf(cpf, "EMPLOYEE")).thenReturn(person);
+        when(mapper.toPersonDTO(person)).thenReturn(dto);
+
+        //ACT
+        service.getPersonByCpf(cpf, "EMPLOYEE");
+
+        //ASSERTIONS
+        verify(repository, times(1)).getPersonByCpf(eq(cpf), eq("EMPLOYEE"));
+    }
+
+    @Test
+    @Order(1)
+    void getPersonByName(){
+        //ARRANGE
+        var persons = (List<Employee>) mock.getListPerson("EMPLOYEE");
+        List<PersonDTO> dtos = mock.getPersonDTOs();
+        var name = "NAME";
+        var type = "EMPLOYEE";
+
+        when(repository.getPersonByName(name, type)).thenAnswer(invocationOnMock -> persons);
+        when(mapper.toListPersonDTO(persons.toArray(new Employee[0]))).thenReturn(dtos);
+
+        //ACT
+
+        service.getPersonByName(name, type);
+
+        //ASSERTIONS
+        verify(repository, atLeastOnce()).getPersonByName(name, type);
+    }
+
+    @Test
+    @Order(2)
+    void insertPersonTest(){
+        //ARRANGE
+
+            var type = "EMPLOYEE";
+            Employee person = (Employee) mock.getPerson(type);
+            var dto = mock.getPersonDTO();
+
+            when(repository.save(person)).thenReturn(person);
+            when(mapper.toPersonDTO(person)).thenReturn(dto);
+            when(mapper.toEmployee(dto)).thenReturn(person);
+        //ACT
+
+            service.insertPerson(type, dto);
+
+        //ASSERTIONS
+
+            verify(repository, times(1)).save(eq(person));
+            verify(mapper, atLeast(1)).toEmployee(dto);
+    }
 }
