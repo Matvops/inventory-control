f9de2bbc80694e10202cc8a9c2cf9f6e0e7f15dd
Matheus <matheusmartinscia@gmail.com>
2024-12-26 23:53:09 -0300

    InsertPurchase and GetAllPurchases methods were added in PersonService class impl

---
+++ src/main/java/com/cadenassi/inventory_control/services/transaction/PurchaseService.java
@@ -0,0 +1,24 @@
+package com.cadenassi.inventory_control.services.transaction;
+
+
+
+import com.cadenassi.inventory_control.dto.objects.purchase.PurchaseDTO;
+
+import java.util.List;
+
+public interface PurchaseService {
+
+    List<PurchaseDTO> getAll();
+
+    PurchaseDTO getPurchaseById(String id);
+
+    List<PurchaseDTO> getPurchaseByDate(String date);
+
+    List<PurchaseDTO> getPurchaseByTimeRange(String first, String last);
+
+    PurchaseDTO insertPurchase(PurchaseDTO purchaseDTO);
+
+    PurchaseDTO addProductById(PurchaseDTO purchaseDTO);
+
+    PurchaseDTO updateDescription(String description);
+}
--- src/main/java/com/cadenassi/inventory_control/services/transaction/PurchaseServiceImpl.java
+++ src/main/java/com/cadenassi/inventory_control/services/transaction/PurchaseServiceImpl.java
@@ -1,7 +1,88 @@
 package com.cadenassi.inventory_control.services.transaction;
 
+import com.cadenassi.inventory_control.dto.mappers.ProductMapper;
+import com.cadenassi.inventory_control.dto.mappers.PurchaseMapper;
+import com.cadenassi.inventory_control.dto.objects.product.ProductDTO;
+import com.cadenassi.inventory_control.dto.objects.purchase.PurchaseDTO;
+import com.cadenassi.inventory_control.exceptions.ResourceNotFoundException;
+import com.cadenassi.inventory_control.repositories.ProductRepository;
+import com.cadenassi.inventory_control.repositories.transaction.PurchaseRepository;
+import com.cadenassi.inventory_control.services.product.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Service
-public class PurchaseServiceImpl {
+public class PurchaseServiceImpl implements PurchaseService{
+
+
+    private static final Logger log = LoggerFactory.getLogger(PurchaseServiceImpl.class);
+
+    @Autowired
+    private PurchaseMapper mapper;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<PurchaseDTO> getAll() {
+        var purchases = mapper.toListPurchaseDTO(purchaseRepository.findAll());
+
+        return purchases;
+    }
+
+    @Override
+    public PurchaseDTO getPurchaseById(String id) {
+        var purchase = mapper.toPurchaseDTO(purchaseRepository
+                .findById(Long.parseLong(id))
+                .orElseThrow(() -> new ResourceNotFoundException("PURCHASE NOT FOUND!")));
+
+        return purchase;
+    }
+
+    @Override
+    public List<PurchaseDTO> getPurchaseByDate(String date) {
+
+        return List.of();
+    }
+
+    @Override
+    public List<PurchaseDTO> getPurchaseByTimeRange(String first, String last) {
+        return List.of();
+    }
+
+
+    @Override
+    public PurchaseDTO insertPurchase(PurchaseDTO purchaseDTO) {
+        log.info("{}", purchaseDTO.getPrice());
+        purchaseDTO.total();
+        log.info("{}", purchaseDTO.getPrice());
+        var purchase = mapper.toPurchase(purchaseDTO);
+        log.info("{}", purchaseDTO.getPrice());
+        var returned = purchaseRepository.save(purchase);
+        var dto = mapper.toPurchaseDTO(returned);
+
+        return dto;
+    }
+
+    @Override
+    public PurchaseDTO addProductById(PurchaseDTO purchaseDTO) {
+
+        return null;
+    }
+
+    @Override
+    public PurchaseDTO updateDescription(String description) {
+        return null;
+    }
 }
